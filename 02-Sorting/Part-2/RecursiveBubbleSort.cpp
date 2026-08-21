2e709d419ae794a69b7986baef2887e32559804b
Aman <aman.ver9345@gmail.com>
2026-08-21 11:15:23 +0530

    solved recursive bubble sort

---
+++ 02-Sorting/Part-2/RecursiveBubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void recursive_bubble_sort(int arr[], int n)
+{
+
+    if (n == 1)
+        return;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+
+    recursive_bubble_sort(arr, n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    recursive_bubble_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
