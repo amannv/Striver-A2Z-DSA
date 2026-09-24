f7aa02bd02fab47f5f4813e1bd7ce67d02e4921d
Aman <aman.ver9345@gmail.com>
2026-09-24 09:28:53 +0530

    solve linear search

---
+++ 03-Arrays/Easy/Part-2/LinearSearch.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LinearSearch(vector<int> arr, int n, int num)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == num)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int num;
+    cin >> num;
+    cout << LinearSearch(arr, n, num);
+}
